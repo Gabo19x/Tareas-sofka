5afdeee474cf953eaf39b2fc2ff63351d8f7116b
Gabriel A Pinzon Paez <arturopinzon19@gmail.com>
2022-04-30 11:49:42 -0500

    Guardando el taller 1 semana 4

---
+++ Ejercicio-pagina/Pagina/app.js
@@ -0,0 +1,44 @@
+var express = require("express"),
+  app = express(),
+  http = require("http"),
+  server = http.createServer(app),
+  mongoose = require("mongoose");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.use(router);
+
+mongoose.connect("mongodb://localhost/tvshows", function (err, res) {
+  if (err) {
+    console.log("ERROR: connecting to Database. " + err);
+  }
+  app.listen(3000, function () {
+    console.log("Node server running on http://localhost:3000");
+  });
+});
+
+var TVShowCtrl = require("./controllers/tvshows");
+
+// API routes
+var tvshows = express.Router();
+
+tvshows
+  .route("/tvshows")
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows
+  .route("/tvshows/:id")
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use("/api", tvshows);
